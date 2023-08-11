24f4efe73066f4c19f37c654dd6152419cd30afd
Mate Soos <mate.soos@ethereum.org>
2023-08-11 13:52:41 +0200

    Making it into a single-solution problem

--- src/unsafe/ds-test/synthetic-manybranch.sol
+++ src/unsafe/ds-test/synthetic-manybranch.sol
@@ -4,21 +4,21 @@ import "forge-std/Test.sol";
 
 
 function pathy(uint256 x) returns(bool) {
-    int256 acc = 1;
+    uint256 acc = 1;
 
     // notice, all the values are primes. So the system has to figure
     // out the primal decomposition of the solution. Hence, there is a unique
     // solution (given that we cannot roll over due to low numbers)
-    acc *= (x & 0xFF > 0)                   ? int256(  1) : int256( 31);
-    acc *= (x & 0x00FF > 0)                 ? int256(  3) : int256( 37);
-    acc *= (x & 0x0000FF > 0)               ? int256(  5) : int256( 41);
-    acc *= (x & 0x000000FF > 0)             ? int256(  7) : int256( 43);
-    acc *= (x & 0x00000000FF > 0)           ? int256( 11) : int256( 47);
-    acc *= (x & 0x0000000000FF > 0)         ? int256( 13) : int256( 53);
-    acc *= (x & 0x000000000000FF > 0)       ? int256( 17) : int256( 59);
-    acc *= (x & 0x00000000000000FF > 0)     ? int256( 19) : int256( 61);
-    acc *= (x & 0x0000000000000000FF > 0)   ? int256( 23) : int256( 67);
-    acc *= (x & 0x000000000000000000FF > 0) ? int256( 29) : int256( 71);
+    acc *= (x & 0xFF000000000000000000 > 0) ? uint256(  1) : uint256( 31);
+    acc *= (x & 0x00FF0000000000000000 > 0) ? uint256(  3) : uint256( 37);
+    acc *= (x & 0x0000FF00000000000000 > 0) ? uint256(  5) : uint256( 41);
+    acc *= (x & 0x000000FF000000000000 > 0) ? uint256(  7) : uint256( 43);
+    acc *= (x & 0x00000000FF0000000000 > 0) ? uint256( 11) : uint256( 47);
+    acc *= (x & 0x0000000000FF00000000 > 0) ? uint256( 13) : uint256( 53);
+    acc *= (x & 0x000000000000FF000000 > 0) ? uint256( 17) : uint256( 59);
+    acc *= (x & 0x00000000000000FF0000 > 0) ? uint256( 19) : uint256( 61);
+    acc *= (x & 0x0000000000000000FF00 > 0) ? uint256( 23) : uint256( 67);
+    acc *= (x & 0x000000000000000000FF > 0) ? uint256( 29) : uint256( 71);
 
     // 31*3*5*7*47*13*59*19*67*71
     // = 10605495576585

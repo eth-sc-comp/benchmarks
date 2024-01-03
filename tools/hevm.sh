f933416a1d5da3437bbbc548ef8d604821ad1491
Mate Soos @ Ethereum Foundation <99662964+msooseth@users.noreply.github.com>
2024-01-03 13:51:38 +0100

    Updating halmos and HEVM to latest `main` (#25)
    * Updating halmos and HEVM
    
    * HEVM should only do 1 iter for loops, like halmos

--- tools/hevm.sh
+++ tools/hevm.sh
@@ -17,9 +17,9 @@ source "$SCRIPT_DIR/utils.sh"
 
 if [[ "${ds_test}" == "0" ]]; then
     code=$(get_runtime_bytecode "${contract_file}" "${contract_name}")
-    out=$(runlim --real-time-limit="${tout}" --space-limit="${memout}" --kill-delay=2 hevm symbolic --code "${code}" --sig "${sig}" "$@" 2>&1)
+    out=$(runlim --real-time-limit="${tout}" --space-limit="${memout}" --kill-delay=2 hevm symbolic --max-iterations=1 --code "${code}" --sig "${sig}" "$@" 2>&1)
 elif [[ "${ds_test}" == "1" ]]; then
-    out=$(runlim --real-time-limit="${tout}" --space-limit="${memout}" --kill-delay=2 hevm test --match "${contract_file}.*${fun_name}" "$@" 2>&1)
+    out=$(runlim --real-time-limit="${tout}" --space-limit="${memout}" --kill-delay=2 hevm test --max-iterations=1 --match "${contract_file}.*${fun_name}" "$@" 2>&1)
 else
     echo "Called incorrectly"
     exit 1

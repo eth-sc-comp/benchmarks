d87a558538d3a15d38b07e1a1f627ec766e24730
dxo <hi@d-xo.org>
2024-01-06 15:20:39 +0100

    add memory synthetics

---
+++ src/unsafe/1tx-abstract/memory-unsafe.sol
@@ -0,0 +1,57 @@
+contract MemoryPropertiesUnsafe {
+    function prove_load_symbolic_index(uint idx) public {
+        uint res;
+        assembly { res := mload(idx) }
+        assert(res == 0);
+    }
+
+    function prove_store_read_same_index_sym(uint idx, uint val) public {
+        uint res;
+        assembly {
+            mstore(idx, val)
+            res := mload(idx)
+        }
+        assert(res != val);
+    }
+
+    function prove_store_read_same_index_conc(uint val) public {
+        uint res;
+        assembly {
+            mstore(1000, val)
+            res := mload(1000)
+        }
+        assert(res != val);
+    }
+
+    function prove_read_non_aligned(uint val) public {
+        uint res;
+        assembly {
+            mstore(0, val)
+            mstore(32, 0)
+            res := mload(16)
+        }
+        assert(res != (val << 128));
+    }
+
+    function prove_read_stacked_write(uint x, uint y) public {
+        uint res;
+        assembly {
+            mstore(0, x)
+            mstore(0, y)
+            res := mload(0)
+        }
+        assert(res != y);
+    }
+
+    function prove_read_stacked_write_non_aligned(uint x, uint y) public {
+        uint res;
+        assembly {
+            mstore(0, x)
+            mstore(16, y)
+            res := mload(0)
+        }
+        uint x_top = (x >> 128) << 128;
+        uint y_bottom = (y << 128) >> 128;
+        assert(res != x_top | y_bottom);
+    }
+}
--- tools/halmos.sh
+++ tools/halmos.sh
@@ -31,6 +31,11 @@ if [[ $out =~ "Counterexample: unknown" ]]; then
   exit 0
 fi
 
+if [[ $out =~ "Counterexample (potentially invalid)" ]]; then
+  echo "result: unknown"
+  exit 0
+fi
+
 if [[ $out =~ "Encountered symbolic CALLDATALOAD offset" ]]; then
   echo "result: unknown"
   exit 0

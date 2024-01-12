75da6cbdbf49b74f7127a8f333a5d2aa03b448e9
dxo <hi@d-xo.org>
2024-01-12 20:26:26 +0100

    big reorg
    - removes a bunch of kinda duplicated stuff
    - moves tests into common files
    - rennames files for consistency

--- src/unsafe/1tx-abstract/arith-unsafe.sol
+++ src/unsafe/1tx-abstract/arith-unsafe.sol
@@ -1,30 +1,30 @@
-contract ArithmeticPropertiesUnchecked {
-    function prove_add_mul_distributivity(uint x, uint y, uint z) public {
+contract ArithmeticPropertiesUncheckedUnsafe {
+    function prove_add_mul_distributivity(uint x, uint y, uint z) public pure {
         unchecked {
             assert(x * (y + y) == (x * y) + (x * z));
         }
     }
-    function prove_sub_comm(uint x, uint y) public {
+    function prove_sub_comm(uint x, uint y) public pure {
         unchecked {
             assert(x - y == y - x);
         }
     }
-    function prove_sub_assoc(uint x, uint y, uint z) public {
+    function prove_sub_assoc(uint x, uint y, uint z) public pure {
         unchecked {
             assert(x - (y - z) == (x - y) - z);
         }
     }
-    function prove_div_distr(uint x, uint y, uint z) public {
+    function prove_div_distr(uint x, uint y, uint z) public pure {
         unchecked {
             assert(x / (y + z) == (x / y) + (x / z));
         }
     }
-    function prove_div_assoc(uint x, uint y, uint z) public {
+    function prove_div_assoc(uint x, uint y, uint z) public pure {
         unchecked {
             assert(x / (y / z) == (x / y) / z);
         }
     }
-    function prove_sub_larger(int x, int y) public {
+    function prove_sub_larger(int x, int y) public pure {
         require(x > y);
         unchecked {
             assert(y - x < 0);
@@ -32,22 +32,31 @@ contract ArithmeticPropertiesUnchecked {
     }
 }
 
-contract ArithmeticPropertiesChecked {
+contract ArithmeticPropertiesCheckedUnsafe {
     // TODO: whats up with hevm here????
-    function prove_sub_comm(uint x, uint y) public {
+    function prove_sub_comm(uint x, uint y) public pure {
         assert(x - y == y - x);
     }
-    function prove_sub_assoc(uint x, uint y, uint z) public {
+    function prove_sub_assoc(uint x, uint y, uint z) public pure {
         assert(x - (y - z) == (x - y) - z);
     }
-    function prove_div_distr(uint x, uint y, uint z) public {
+    function prove_div_distr(uint x, uint y, uint z) public pure {
         assert(x / (y + z) == (x / y) + (x / z));
     }
-    function prove_div_assoc(uint x, uint y, uint z) public {
+    function prove_div_assoc(uint x, uint y, uint z) public pure {
         assert(x / (y / z) == (x / y) / z);
     }
     function prove_sdiv_comm(int a, int b) public pure {
         require(a != 0 && b != 0, "One of the values is zero");
         assert(a / b == b / a);
+    }
+    function prove_distributivity(uint120 x, uint120 y, uint120 z) public pure {
+        assert(x + (y * z) == (x + y) * (x + z));
+    }
+    function prove_add2(uint x, uint y) public pure {
+        assert(x + y >= x);
+    }
+    function prove_complicated(uint x, uint y, uint z) public pure {
+        assert((((x * y) / z) * x) / (x * y * z) == (((x * y) + z) / x) * (y / z));
     }
 }

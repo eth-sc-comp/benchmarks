38d34025eb1617cbf805795340b3eb4afa1bc96c
dxo <hi@d-xo.org>
2024-01-14 13:49:57 +0100

    add amm example

--- src/common/erc20.sol
+++ src/common/erc20.sol
@@ -26,12 +26,6 @@ contract ERC20 {
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
-    // --- eip2612 data ---
-
-    uint256 internal immutable INITIAL_CHAIN_ID;
-    bytes32 internal immutable INITIAL_DOMAIN_SEPARATOR;
-    mapping(address => uint256) public nonces;
-
     // --- admin ---
 
     address public owner;
@@ -47,9 +41,6 @@ contract ERC20 {
         name = _name;
         symbol = _symbol;
         decimals = _decimals;
-
-        INITIAL_CHAIN_ID = block.chainid;
-        INITIAL_DOMAIN_SEPARATOR = computeDomainSeparator();
 
         owner = msg.sender;
     }
@@ -103,81 +94,6 @@ contract ERC20 {
     // --- mint / burn logic ---
 
     function mint(address to, uint256 amount) public auth {
-        _mint(to, amount);
-    }
-
-    function burn(address from, uint256 amount) public auth {
-        _burn(from, amount);
-    }
-
-    // --- eip2612 logic ---
-
-    function permit(
-        address owner,
-        address spender,
-        uint256 value,
-        uint256 deadline,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) public {
-        require(deadline >= block.timestamp, "PERMIT_DEADLINE_EXPIRED");
-
-        // Unchecked because the only math done is incrementing
-        // the owner's nonce which cannot realistically overflow.
-        unchecked {
-            address recoveredAddress = ecrecover(
-                keccak256(
-                    abi.encodePacked(
-                        "\x19\x01",
-                        DOMAIN_SEPARATOR(),
-                        keccak256(
-                            abi.encode(
-                                keccak256(
-                                    "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
-                                ),
-                                owner,
-                                spender,
-                                value,
-                                nonces[owner]++,
-                                deadline
-                            )
-                        )
-                    )
-                ),
-                v,
-                r,
-                s
-            );
-
-            require(recoveredAddress != address(0) && recoveredAddress == owner, "INVALID_SIGNER");
-
-            allowance[recoveredAddress][spender] = value;
-        }
-
-        emit Approval(owner, spender, value);
-    }
-
-    function DOMAIN_SEPARATOR() public view returns (bytes32) {
-        return block.chainid == INITIAL_CHAIN_ID ? INITIAL_DOMAIN_SEPARATOR : computeDomainSeparator();
-    }
-
-    function computeDomainSeparator() internal view returns (bytes32) {
-        return
-            keccak256(
-                abi.encode(
-                    keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
-                    keccak256(bytes(name)),
-                    keccak256("1"),
-                    block.chainid,
-                    address(this)
-                )
-            );
-    }
-
-    // --- internal mint / burn logic ---
-
-    function _mint(address to, uint256 amount) internal {
         totalSupply += amount;
 
         // Cannot overflow because the sum of all user
@@ -189,7 +105,7 @@ contract ERC20 {
         emit Transfer(address(0), to, amount);
     }
 
-    function _burn(address from, uint256 amount) internal {
+    function burn(address from, uint256 amount) public auth {
         balanceOf[from] -= amount;
 
         // Cannot underflow because a user's balance
